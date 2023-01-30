368ca036ad0296e1a742df96511de73177297281
Oniso20 <emem.c.onis@gmail.com>
2023-01-30 12:57:11 +0200

    Add Queue component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Queue from "./Queue";
 
 function App() {
   return (
     <div className="App">
       <h1> Jenkins Group Task</h1>
+      <Queue />
     </div>
   );
 }
